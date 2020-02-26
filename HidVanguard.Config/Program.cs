2db3bcfa738c9cc714e07f024ba337e245ecf801
T E Dixon <dixonte@gmail.com>
2020-02-26 23:38:18 +1100

    Added some lazy error trapping

--- HidVanguard.Config/Program.cs
+++ HidVanguard.Config/Program.cs
@@ -18,17 +18,32 @@ namespace HidVanguard.Config
         [STAThread]
         static int Main(string[] args)
         {
-            using (var container = new WindsorContainer())
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            try
             {
-                // Registration
-                container.Install(FromAssembly.Containing<CompositionRoot>());
-                
+                using (var container = new WindsorContainer())
+                {
+                    // Registration
+                    container.Install(FromAssembly.Containing<CompositionRoot>());
 
-                // Bootstrap
-                var root = container.Resolve<ICompositionRoot>();
+                    // Bootstrap
+                    var root = container.Resolve<ICompositionRoot>();
 
-                return root.Run(args);
+                    return root.Run(args);
+                }
             }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString(), "HidVanguard.Config Main Exception", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+
+            return -1;
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            System.Windows.MessageBox.Show(e.ExceptionObject.ToString(), "HidVanguard.Config Unhandled Exception", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
         }
     }
 }
